75d9240943ca24dc8d89b225a83a6bf6a1e293f2
Carl Lundholm <carl_0221@hotmail.se>
2025-06-05 11:51:39 +0200

    Test

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -32,7 +32,7 @@ const AuthProvider = ({ children }) => {
             localStorage.setItem("refreshToken", res.refreshToken);
             navigate("/today");
         } catch (error) {
-            throw error; // Re-throw the error to be handled by the caller
+            throw error; // Re-throw the error to be handled by the call
         }
     };
 
