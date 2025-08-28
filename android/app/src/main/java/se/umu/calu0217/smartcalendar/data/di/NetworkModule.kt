3970b8cb4a860abe58e5943b7b9f3776aecaa8f7
Carl Lundholm <carl_0221@hotmail.se>
2025-08-28 15:38:41 +0200

    Add Hilt network setup and base URL config

---
+++ android/app/src/main/java/se/umu/calu0217/smartcalendar/data/di/NetworkModule.kt
@@ -0,0 +1,36 @@
+package se.umu.calu0217.smartcalendar.data.di
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import se.umu.calu0217.smartcalendar.BuildConfig
+import se.umu.calu0217.smartcalendar.data.LocalDateTimeAdapter
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(LocalDateTimeAdapter())
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+}
+
--- android/app/src/main/java/se/umu/calu0217/smartcalendar/data/repository/UserRepository.kt
+++ android/app/src/main/java/se/umu/calu0217/smartcalendar/data/repository/UserRepository.kt
@@ -3,17 +3,20 @@ package se.umu.calu0217.smartcalendar.data.repository
 import android.content.Context
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import se.umu.calu0217.smartcalendar.BuildConfig
 import se.umu.calu0217.smartcalendar.data.TokenDataStore
 import se.umu.calu0217.smartcalendar.data.api.UserApi
 import se.umu.calu0217.smartcalendar.domain.ActivityStatsDTO
 import se.umu.calu0217.smartcalendar.domain.TaskStatsDTO
 import se.umu.calu0217.smartcalendar.domain.UserDTO
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class UserRepository(context: Context) {
+class UserRepository @Inject constructor(@ApplicationContext context: Context) {
     private val dataStore = TokenDataStore(context)
 
     private val api: UserApi = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:8080/api/")
+        .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
         .create(UserApi::class.java)
